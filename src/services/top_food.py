ebd79415f46bd04650130f1129a5fdb21335b4cb
alexandremuzio <ax.muzio@gmail.com>
2017-12-09 16:44:17 +0000

    Adding working food recomender model

--- src/services/top_food.py
+++ src/services/top_food.py
@@ -1,35 +1,62 @@
 from utils import nice_json
 from flask import Flask, request
-import pandas
+import pandas as pd
 
-import pickle
+from sklearn.externals import joblib
 
-MODEL_PATH = "/home/ubuntu/dl_hackathon/data/food-101/"
-df = pandas.read_csv(csv_path)
+NUT_DAILY_VALUES = {
+    "calories": 2000,
+    "carbohydrates": 300,
+    "cholesterol": 0.003,
+    "fat" : 65,
+    "fiber": 25,
+    "proteins": 50
+}
 
-products = df['product_name'].unique()
+NUT_NAMES_MAP = {
+    "calories" : "energy_100g",
+    "carbohydrates": "carbohydrates_100g",
+    "cholesterol": "cholesterol_100g",
+    "fat" : "fat_100g",
+    "fiber": "fiber_100g",
+    "proteins": "proteins_100g"
+}
+
+MODELS_PATH = "/home/ubuntu/dl_hackathon/data/food-101/models/"
+
+nutrition_df = pd.read_csv(MODELS_PATH + "../../nutrition_values.csv", header=0)
+
+knn = joblib.load(MODELS_PATH + "knn.pkl")
+
 
 app = Flask(__name__)
 
 @app.route("/",  methods=['POST'])
 def similar_foods():
-    product_name = request.args.get('product_name')
-    calories = request.args.get('calories')
-    carbohydrates = request.args.get('cholesterol')
-    fat = request.args.get('fat')
-    fiber = request.args.get('fiber')
-    proteins = request.args.get('proteins')
-    # check if the post request has the file part
-    if 'file' not in request.files:
-        flash('No file part')
-        return redirect(request.url)
+    product_name = request.form.get('product_name')
+    calories = float(request.form.get('calories'))
+    cholesterol = float(request.form.get('cholesterol'))
+    carbohydrates = float(request.form.get('carbohydrates'))
+    fat = float(request.form.get('fat'))
+    fiber = float(request.form.get('fiber'))
+    proteins = float(request.form.get('proteins'))
     
-    if product_name not in products:
-        return nice_json({})
-    dic = df.loc[df['product_name'] == product_name].to_dict()
-    for key, val in dic.items():
-        dic[key] = val[next(iter(val))]
-    return nice_json(dic)
+    # TODO Check if any parameter is None
+    
+    distances, indices = knn.kneighbors([[NUT_DAILY_VALUES["calories"] - calories,
+                                          NUT_DAILY_VALUES["carbohydrates"] - carbohydrates,
+                                          NUT_DAILY_VALUES["proteins"] - proteins,
+                                          NUT_DAILY_VALUES["fat"] - fat,
+                                          NUT_DAILY_VALUES["fiber"] - fiber,
+                                          NUT_DAILY_VALUES["cholesterol"] - cholesterol]],
+                                        n_neighbors=3)
+    
+    recommended_products = [nutrition_df.loc[i]['product_name'] for i in indices[0]]
+
+    response_dict = {
+        "recommended": recommended_products
+    }
+    return nice_json(response_dict)
 
 if __name__=="__main__":
     app.run(port=5002, debug=True)
